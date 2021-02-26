3d3371d670bba3872e8ec30b90b9efd200259986
Shwetahe <shwetahe@thoughtworks.com>
2021-02-26 11:12:37 +0530

    Issue5|Shweta - Adding option to start selenoid on any port

--- src/cli.js
+++ src/cli.js
@@ -1,9 +1,18 @@
 #!/usr/bin/env node
+import logger from './logger';
+
+require
 const { promisify } = require('util');
 const exec = promisify(require('child_process').exec);
 var spawn = require('child_process').spawn;
 const chalk = require('chalk');
 const log = console.log;
+const defaultPort = 4444;
+let selenoidPort;
+if (fs.existsSync(path.resolve(__dirname, '../selenoid.config.js'))) {
+  let selenoidConfig = require('../selenoid.config');
+  selenoidPort = selenoidConfig.selenoidPort ? selenoidConfig.selenoidPort : defaultPort;
+}
 
 class SelenoidSetup {
   async downloadCM() {
@@ -22,11 +31,11 @@ class SelenoidSetup {
       logger.error(e);
     }
     const ls = spawn('./cm', ['selenoid', 'configure', '--browsers', 'chrome:86.0', '-f']);
-    ls.stdout.on('data', function(data) {
+    ls.stdout.on('data', function (data) {
       log(chalk.blue(data.toString()));
     });
     ls.on('error', async () => await this.downloadCM());
-    ls.stderr.on('data', function(data) {
+    ls.stderr.on('data', function (data) {
       log(chalk.cyan(data.toString()));
     });
 
@@ -34,23 +43,23 @@ class SelenoidSetup {
   }
 
   startSelenoid() {
-    log(chalk.greenBright('Starting Selenoid....'));
-    const ls = spawn('./cm', ['selenoid', 'start', '--vnc']);
-    ls.stdout.on('data', function(data) {
+    log(chalk.greenBright(`Starting Selenoid on port: ${port}`));
+    const ls = spawn('./cm', ['selenoid', 'start', '--vnc', '--port', `${port}`]);
+    ls.stdout.on('data', function (data) {
       console.log(chalk.blue(data.toString()));
     });
 
-    ls.stderr.on('data', function(data) {
+    ls.stderr.on('data', function (data) {
       console.log(chalk.cyan(data.toString()));
     });
     ls.on('exit', () => {
       log(chalk.greenBright('Starting Selenoid UI'));
       const ls = spawn('./cm', ['selenoid-ui', 'start']);
-      ls.stdout.on('data', function(data) {
+      ls.stdout.on('data', function (data) {
         log(chalk.blueBright(data.toString()));
       });
 
-      ls.stderr.on('data', function(data) {
+      ls.stderr.on('data', function (data) {
         log(chalk.gray(data.toString()));
       });
     });
